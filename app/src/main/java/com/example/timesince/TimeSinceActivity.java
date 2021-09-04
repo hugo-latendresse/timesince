02ef36007ed24398c5033963c8240193bb95a3ae
HugoLatendresse <Hugo.Latendresse@oliverwyman.com>
2021-09-04 16:55:46 -0400

    3 numbers input works

--- app/src/main/java/com/example/timesince/TimeSinceActivity.java
+++ app/src/main/java/com/example/timesince/TimeSinceActivity.java
@@ -2,50 +2,61 @@ package com.example.timesince;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.timesince.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TimeSinceActivity extends AppCompatActivity {
-    private TextView message;
-    private int counter = 0;
+    private TextView input1;
+    private EditText input2;
+    private EditText input3;
+    private TextView tv_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        message = findViewById(R.id.clickCounter);
-        ImageView droid = findViewById(R.id.droidImage);
+        input1 = (EditText) findViewById(R.id.et_input1);
+        input2 = (EditText) findViewById(R.id.et_input2);
+        input3 = (EditText) findViewById(R.id.et_input3);
+    Button bt_calculate = (Button) findViewById(R.id.bt_calculate);
 
-        //Define and attach click listener
-        droid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tapDroid();
-            }
-        });
-    }
+    tv_result = (TextView) findViewById(R.id.tv_result);
 
-    private void tapDroid() {
-        counter++;
-        String countAsText;
-        /*
-         * In real applications you should not write switch like the one below.
-         * Use resource of type "Quantity strings (plurals)" instead.
-         * See https://developer.android.com/guide/topics/resources/string-resource#Plurals
-         */
-        switch (counter) {
-            case 1:
-                countAsText = "once";
-                break;
-            case 2:
-                countAsText = "twice";
-                break;
-            default:
-                countAsText = String.format("%d times", counter);
+        bt_calculate.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            makeCalculations();
         }
-        message.setText(String.format("You touched the droid %s", countAsText));
-    }
+    });
 }
+
+    private void makeCalculations() {
+        // I'm assuming you're getting numbers.
+        double n1 = Double.valueOf(input1.getText().toString());
+        double n2 = Double.valueOf(input2.getText().toString());
+        double n3 = Double.valueOf(input3.getText().toString());
+
+        // Do your calculation here.
+        // I'm assuming you have inserted the result on a variable called 'result'. Like: double result
+        double result = n2-n1;
+        tv_result.setText("The result is: " + result);
+    }
+
+    // The rest of your Activity and methods.
+
+}
+
+//    private void setTodaysDate() {
+//        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm");
+//        Date date = new Date(System.currentTimeMillis());
+//        String out;
+//        out = "Produced" + (formatter).format(date);
+//        input1.setText("Clean since: "+ out);
+////        System.out.println(formatter.format(date));
+//    }
